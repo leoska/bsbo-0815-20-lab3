a02798f55d807d62ed3df306fb79cc1e91c803d9
Алла Гальперина <alla.galperina152436@gmail.com>
2021-11-06 19:24:22 +0300

    Added Node and Tree class

---
+++ bsbo-0815-20-lab3/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bsbo_0815_20_lab3
+{
+    class Node
+    {
+        int value;
+        Node left_ch;
+        Node right_bro;
+       public Node(int NewValue)
+        {
+            value = NewValue;
+        }
+    }
+}
---
+++ bsbo-0815-20-lab3/Tree.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bsbo_0815_20_lab3
+{
+    class Tree
+    {
+        private Node Head;
+
+        public void Add(int value)
+        {
+            if (Head==null)
+            {
+                Head = new Node(value);
+
+            }
+        }
+    }
+}
