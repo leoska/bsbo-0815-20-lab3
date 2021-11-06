b2ac1300aedf9c305842c0289780f559453c3037
leoska <leo77551@gmail.com>
2021-11-06 18:49:33 +0300

    WIP

---
+++ bsbo-0815-20-lab3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace bsbo_0815_20_lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Дерево двоичного поиска
+            // Левый сын, правый брат (указатели)
+            // А=A ⋃прB
+            // А–обратный, В – симметричный
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
